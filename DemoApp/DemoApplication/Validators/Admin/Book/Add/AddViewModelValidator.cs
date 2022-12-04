dd60e6f6a30c5f09db963ca4d0768910fee1e16c
Zphorium <106176680+Zphorium@users.noreply.github.com>
2022-12-04 17:57:02 +0400

    Navbar And SubNavbar structure updated

---
+++ DemoApp/DemoApplication/Validators/Admin/Book/Add/AddViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using DemoApplication.ViewModels.Admin.Navbar;
+using FluentValidation;
+
+namespace DemoApplication.Validators.Admin.Navbar.Add
+{
+    public class AddViewModelValidator : AbstractValidator<AddViewModel>
+    {
+        public AddViewModelValidator()
+        {
+            RuleFor(avm => avm.Name)
+                .NotNull()
+                .WithMessage("Title can't be empty")
+                .NotEmpty()
+                .WithMessage("Title can't be empty")
+                .MinimumLength(5)
+                .WithMessage("Minimum length should be 5")
+                .MaximumLength(25)
+                .WithMessage("Maximum length should be 25");
+        }
+    }
+}
